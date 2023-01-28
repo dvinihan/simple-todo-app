fc1e4f29e46f761d7adea03d3b78549d1803f10b
Daniel Vinihan <daniel.vinihan@gmail.com>
2023-01-28 14:57:20 -0600

    add test mongo uri

--- util/mongodb.ts
+++ util/mongodb.ts
@@ -1,8 +1,8 @@
 import { MongoClient } from "mongodb";
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Invalid environment variable: "MONGODB_URI"');
-}
+const globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise: Promise<MongoClient>;
+};
 
 const uri = process.env.MONGODB_URI;
 const options = {};
@@ -10,18 +10,18 @@ const options = {};
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!(global as any)._mongoClientPromise) {
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
