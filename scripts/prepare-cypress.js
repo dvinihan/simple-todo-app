227b8e6a76faea265826f61f721a90d4bebd1577
Daniel Vinihan <daniel.vinihan@gmail.com>
2023-02-09 15:37:02 -0600

    prepareCypress script

---
+++ scripts/prepare-cypress.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { writeFileSync, readFileSync } = require("fs");
+
+const prepareCypress = () => {
+  const envBuffer = readFileSync(".env.test.local");
+  const envString = envBuffer.toString();
+  const envLines = envString.split("\n");
+  const mongoLine = envLines.find((line) => line.startsWith("MONGODB_URI"));
+  const mongoUriIndex = mongoLine.indexOf("mongodb+srv://");
+  const mongoUri = mongoLine.slice(mongoUriIndex);
+
+  writeFileSync(
+    "cypress.env.json",
+    `{"mongodb":{"uri":"${mongoUri}","database":"simple-cleaning-app"}}`
+  );
+};
+
+prepareCypress();
