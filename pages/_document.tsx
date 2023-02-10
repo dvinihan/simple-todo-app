0cf45dbfb75aba09bf5af072d491a7f83fda7165
Daniel Vinihan <daniel.vinihan@gmail.com>
2023-02-09 21:19:24 -0600

    make pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link href="/manifest.json" rel="manifest" />
+        <link href="/favicon.ico" rel="apple-touch-icon"></link>
+      </Head>
       <body style={{ backgroundColor: "lightblue" }}>
         <Main />
         <NextScript />
